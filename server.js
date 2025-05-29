afac2cf2d8fc1a1c9a0ae5a85b9adfdcbf23b954
Ak00021 <131507961+Ak00021@users.noreply.github.com>
2025-05-30 02:18:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://yourfrontend.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 //importing api routes
 const userApi = require('./API/UserApi');
